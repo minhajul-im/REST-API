c452aabb1975a199a07e6b3d345e2d9896cf1b59
minhajul-im <minhajul.cpp@gmail.com>
2024-01-31 17:07:48 +0600

    slightly update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,12 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <link
-        className='rounded-full w-14 h-14'
-        rel='shortcut icon'
-        href='/logo.jpg'
-        type='image/x-icon'
-      />
+      <link rel='shortcut icon' href='/logo.jpg' type='image/x-icon' />
       <body className={"bg-[#283469]"}>{children}</body>
     </html>
   );
