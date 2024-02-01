23be06a447e16f70782e152bb29b42f694dabb0e
minhajul-im <minhajul.cpp@gmail.com>
2024-02-01 16:47:35 +0600

    update link

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,11 +5,7 @@ export default function Social() {
     <div className='mt-[95%] md:mt-[65%] lg:mt-[50%] xl:mt-[40%]'>
       <p className='flex mt-28 md:mt-0 lg:mt-0 justify-center items-baseline gap-6'>
         {soicals.map((social) => (
-          <a
-            key={social.id}
-            href={"https://wa.me/+8801301109244"}
-            target='_blank'
-          >
+          <a key={social.id} href={social.link} target='_blank'>
             <social.icon
               className='text-3xl md:text-4xl lg:text-5xl text-white transition-all
              hover:text-[#4267B2] bg-[4267B2] border border-[#4267B2] rounded-full hover:bg-white'
