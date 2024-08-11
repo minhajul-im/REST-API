e6b44f60cabb0d3a234a0ffbb1cff9a2ec877eba
minhajul <dev.minhajul@gmail.com>
2024-08-11 22:09:25 +0600

    added all mock data

---
+++ src/constant/constant.ts
@@ -0,0 +1,5 @@
+const emailTemplateId = import.meta.env.VITE_EMAIL_TEMPLATE_ID;
+const emailServiceId = import.meta.env.VITE_EMAIL_SERVICE_ID;
+const emailPublicKey = import.meta.env.VITE_EMAIL_PUBLIC_KEY;
+
+export { emailServiceId, emailTemplateId, emailPublicKey };
