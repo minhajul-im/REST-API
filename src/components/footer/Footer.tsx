948c7a341a304ed35c63cbfb06cbaaca96b247e8
minhajul <dev.minhajul@gmail.com>
2024-08-15 10:41:21 +0600

    test footer section but i can't check allsocial icons

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,38 +1,38 @@
 import "./Footer.css";
+import React from "react";
 import { IconType } from "../../icons";
-import { SOCIALS, SOCIAL_LINKS } from "../../constant/mock-data";
+import SocialIcons from "./SocialIcons";
 
 const Footer = ({ isDark }: IconType) => {
+  console.log(isDark);
+
   return (
-    <footer className="footer">
-      <div className="waves">
-        <div className="wave" id="wave1"></div>
-        <div className="wave" id="wave2"></div>
-        <div className="wave" id="wave3"></div>
-        <div className="wave" id="wave4"></div>
-      </div>
-      <h2 className="text-2xl font-fontBold text-TX-soft text-center py-4">
-        Follow Us
-      </h2>
-      <div className="flex gap-4 pb-8 items-center">
-        {SOCIALS.map((ICON, idx: number) => (
-          <a className="text-white" key={idx} href={SOCIAL_LINKS[idx]}>
-            <ICON isDark={isDark} />
-          </a>
-        ))}
-      </div>
+    <React.Fragment>
+      <footer className="footer">
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
+        <h2 className="text-2xl font-fontBold text-TX-soft text-center py-4">
+          Follow Us
+        </h2>
 
-      <div className="flex gap-6 items-center text-TX-soft font-fontMed pb-4 text-base">
-        <button className="hover:text-TX-dark transition-all duration-100">
-          Terms & Conditions
-        </button>
-        <button className="hover:text-TX-dark transition-all duration-100">
-          Privacy & Policy
-        </button>
-      </div>
+        <SocialIcons isDark={isDark} />
 
-      <p className="italic text-sm">&copy;2024 packNjar </p>
-    </footer>
+        <div className="flex gap-6 items-center text-TX-soft font-fontMed pb-4 text-base">
+          <button className="hover:text-TX-dark transition-all duration-100">
+            Terms & Conditions
+          </button>
+          <button className="hover:text-TX-dark transition-all duration-100">
+            Privacy & Policy
+          </button>
+        </div>
+
+        <p className="italic text-sm">&#169;2024 packNjar </p>
+      </footer>
+    </React.Fragment>
   );
 };
 
