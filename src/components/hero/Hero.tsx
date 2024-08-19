188403cdf98411e7dc1e08ec0c114f0d7edc7c68
minhajul <dev.minhajul@gmail.com>
2024-08-19 19:30:03 +0600

    checking email sending

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Title from "./Title";
+// import Title from "./Title";
 import { thumbnail, video } from "../../constant/mock-data";
 
 const Hero = () => {
@@ -7,9 +7,11 @@ const Hero = () => {
     <React.Fragment>
       <section
         id="hero"
+        data-testid="hero"
         className="flex h-screen items-center justify-center relative bg-[#283469] z-10">
         <div className="absolute inset-0 -z-20 w-full h-full overflow-hidden">
           <video
+            data-testid="hero-video"
             src={video}
             className="w-full h-full md:object-cover"
             muted
@@ -19,11 +21,7 @@ const Hero = () => {
             poster={thumbnail}></video>
         </div>
 
-        <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-60% to-black" />
-
-        <div className="relative z-20 flex flex-col h-screen justify-end pb-16 md:pb-12 mx-4 w-full md:w-[750px] lg:w-[900px] text-center transition-all duration-200">
-          <Title />
-        </div>
+        {/* <Title /> */}
       </section>
     </React.Fragment>
   );
