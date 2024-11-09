e62b6c644d36bd4c19d85001e86c5f3245a632dd
minhajul <dev.minhajul@gmail.com>
2024-11-09 20:51:16 +0600

    added metadata and products page done

--- src/pages/feature-product/screen-product-page.tsx
+++ src/pages/feature-product/screen-product-page.tsx
@@ -1,3 +1,11 @@
+import { Title } from "@/components/common/title";
+import { AllProductFeature } from "./all-products-feature/all-products-feature";
+
 export const ScreenProductsPage = () => {
-  return <div>ScreenProductsPage</div>;
+  return (
+    <section className="layout-container mb-20">
+      <Title>Our Nic Out Products</Title>
+      <AllProductFeature />
+    </section>
+  );
 };
