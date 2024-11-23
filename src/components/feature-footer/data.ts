2870786ecf791bef220edfc71631d91c766409dd
minhajul <dev.minhajul@gmail.com>
2024-11-23 09:39:42 +0600

    feat: privacy and policy, terms and condition feature added

---
+++ src/components/feature-footer/data.ts
@@ -0,0 +1,17 @@
+export const PRIVACY_POLICY: string = `Nicotine replacement products help relieve some of the withdrawal symptoms people experience when they quit smoking. Three products for nicotine replacement therapy (NRT) are currently available over-the-counter in the U.S., including two nicotine patches and nicotine gum. Nicotine nasal spray and a nicotine inhaler are available only by prescription. 
+A new non-nicotine pill is also available as a smoking cessation option. An aqua filter cigarette holder (available in drug stores) can reduce the tar and irritation to your throat even if you’re not ready to quit completely. 
+To be most effective, nicotine replacement products should be used in conjunction with a behavior change program. The goal in using nicotine medications is to stop smoking completely. If you plan to take nicotine medications, begin using them on the day you quit. If you continue to have strong urges to smoke or are struggling to stop smoking completely, ask your health care provider about additional help.`;
+
+export const TERMS_CONDITION: string = `If for any reason, you are not completely satisfied with any product you are entitled to return it to us within 30 days of receipt. You-ll be refunded the full purchase price or receive full credit toward future purchase of our product. Unfortunately, we cannot return shipping charges. This guarantee does not apply to any product intentionally damaged, marked or misused. Before returning your purchase, please E-mail us for instructions.`;
+
+export type Contact = {
+  phone: string;
+  email: string;
+  address: string;
+};
+
+export const contact: Contact = {
+  phone: "+880 14 ** 00 ** **",
+  email: "info@packnjar.com",
+  address: "Uttara, Dhaka-1230",
+};
--- src/components/feature-footer/feature-bottom/feature-bottom.tsx
+++ src/components/feature-footer/feature-bottom/feature-bottom.tsx
@@ -2,50 +2,50 @@
 
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { buttonVariants } from "@/components/ui/button";
 import { CustomDialog } from "@/components/common/custom-dialog";
+import { Paragraph } from "@/components/common/paragraph";
+import { PRIVACY_POLICY, TERMS_CONDITION } from "../data";
 
 export const FeatureBottom = () => {
   const path = usePathname();
   const [open, setOpen] = useState("");
 
   return (
-    <React.Fragment>
-      <section className="flex justify-center items-center flex-col-reverse md:flex-row md:justify-between py-5 border-t">
-        <p className="text-sm text-blue-600 text-center md:text-start pt-6 md:p-0">
-          &#169;2024 packnjar
-        </p>
-        <ul className="flex gap-2 md:gap-4 flex-col md:flex-row justify-center md:justify-start items-center">
-          <li className={cn(buttonVariants({ variant: "link" }))}>
-            <Link
-              href="/faq"
-              className={`text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-500 ${
-                path === "/faq" ? "text-blue-500" : ""
-              }`}>
-              F.A.Q
-            </Link>
-          </li>
-          <CustomDialog
-            linkText="Privacy Policy"
-            title="Privacy Policy"
-            open={open === "POLICY"}
-            onClose={() => setOpen("")}
-            onOpen={() => setOpen("POLICY")}>
-            <div>policy</div>
-          </CustomDialog>
+    <section className="flex justify-center items-center flex-col-reverse md:flex-row md:justify-between py-5 border-t">
+      <p className="text-sm text-blue-600 text-center md:text-start pt-6 md:p-0">
+        &#169;2024 packnjar
+      </p>
+      <ul className="flex gap-2 md:gap-4 flex-col md:flex-row justify-center md:justify-start items-center">
+        <li className={cn(buttonVariants({ variant: "link" }))}>
+          <Link
+            href="/faq"
+            className={`text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-500 ${
+              path === "/faq" ? "text-blue-500" : ""
+            }`}>
+            F.A.Q
+          </Link>
+        </li>
+        <CustomDialog
+          linkText="Privacy Policy"
+          title="Privacy Policy"
+          open={open === "POLICY"}
+          onClose={() => setOpen("")}
+          onOpen={() => setOpen("POLICY")}>
+          <Paragraph>{PRIVACY_POLICY}</Paragraph>
+        </CustomDialog>
 
-          <CustomDialog
-            linkText="Terms & Conditions"
-            title="Terms & Conditions"
-            open={open === "TERM"}
-            onClose={() => setOpen("")}
-            onOpen={() => setOpen("TERM")}>
-            <div>term</div>
-          </CustomDialog>
-        </ul>
-      </section>
-    </React.Fragment>
+        <CustomDialog
+          linkText="Terms & Conditions"
+          title="Terms & Conditions"
+          open={open === "TERM"}
+          onClose={() => setOpen("")}
+          onOpen={() => setOpen("TERM")}>
+          <Paragraph>{TERMS_CONDITION}</Paragraph>
+        </CustomDialog>
+      </ul>
+    </section>
   );
 };
